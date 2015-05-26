ebf844d19246c2827a8b6711802622237af7e249
oldDadou <devnull@localhost>
2015-05-26 14:29:35 +0200

    initial commit

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <mpi.h>
 #include <stdio.h>
 
+typedef void (*CAN_Callback)(void* data);
 
-int main(int arc, char**argv) {
-  printf("hello world !");
+void callback_test_no_args(void *dummy) {
+  printf("called \n");
+}
+
+void callback_test_string(void *data) {
+  printf("called %d\n", *((int*)data));
+}
+
+int main(int argc, char**argv) {
+  MPI_Init (&argc, &argv);
+  int nb_proc, com_rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &com_rank);
+  int i = 4;
+  CAN_Callback cb[2];
+  cb[0] = &callback_test_no_args;
+  cb[1] = &callback_test_string;
+
+  cb[0](NULL);
+  cb[1](&i);
+
+  MPI_Finalize();
+  return 0;
 }
