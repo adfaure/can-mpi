2dc652e75a4040b73f00ffe81565969890681e72
oldDadou <devnull@localhost>
2015-05-27 10:12:45 +0200

    base message to join the overlay

--- src/main.c
+++ src/main.c
@@ -11,10 +11,12 @@
 //
 #define ACK_TAG 42
 #define REQUEST_TO_JOIN 666
+#define NEGOCIATE_LAND  123
+#define LOCALIZE 321
+#define LOCALIZE_RESP 13
 
 #define SIZE_X 1000
 #define SIZE_Y 1000
-
 
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
@@ -38,6 +40,11 @@ typedef enum { false, true } bool;
 void get_random_id(pair *p, int min, int max);
 
 /**
+ * Init a pair
+ */
+void init_pair(pair *, int x, int y);
+
+/**
  * affiche une pair sur la sortie standard
  */
 void print_pair(const pair *p);
@@ -46,6 +53,11 @@ void print_pair(const pair *p);
  * return true if the land cotains the point defined by x and y is
  */
 bool is_land_contains(const land *l, int x, int y);
+
+/**
+ * return true if the land contains the pair
+ */
+bool is_land_contains_pair(const land *l,const pair *p);
 
 /**
  * split old_land and init new_land initialized to the half of old_land
@@ -78,12 +90,13 @@ int **alloc_2d_int(int rows, int cols);
 float **alloc_2d_float(int rows, int cols);
 
 int main(int argc, char**argv) {
-  int nb_proc, com_rank,node_number ,buffer_int, i;
-  int main_loop_tag, main_loop_from;
+  int nb_proc, com_rank,node_number , i, corresp, localise;
+  int main_loop_tag, main_loop_from, count, *main_loop_buffer_int = NULL, *send_int_buffer;
   bool bootstrap = false, active = false;
-  pair pair_id;
-  land land_id;
+  pair pair_id, pair_join_request;
+  land land_id, new_land;
   MPI_Status main_loop_status;
+  MPI_Request main_loop_request;
   MPI_Init (&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
   MPI_Comm_rank(MPI_COMM_WORLD, &com_rank);
@@ -91,41 +104,67 @@ int main(int argc, char**argv) {
   srand((unsigned) time(NULL) + com_rank * nb_proc);
 
   if(com_rank == ROOT_PROCESS) {
+    main_loop_buffer_int = (int*) malloc(sizeof(int));
     for(i = 0; i < nb_proc; i++) {
       if(i != ROOT_PROCESS) {
         MPI_Send(&i, 1, MPI_INT, i, ROOT_TAG_INIT_NODE, MPI_COMM_WORLD);
-        MPI_Recv(&buffer_int ,1 , MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &main_loop_status);
+        MPI_Recv(&main_loop_buffer_int[0] ,1 , MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &main_loop_status);
         main_loop_from = main_loop_status.MPI_SOURCE;
         main_loop_tag  = main_loop_status.MPI_TAG;
         if(main_loop_tag == ACK_TAG) {
           printf("bootstraping node did it well \n");
         } else if(main_loop_tag == REQUEST_TO_JOIN) {
-          buffer_int = 1;
-          MPI_Send(&buffer_int, 1, MPI_INT, i, REQUEST_TO_JOIN, MPI_COMM_WORLD );
+          *main_loop_buffer_int = 1;
+          MPI_Send(&main_loop_buffer_int[0] , 1, MPI_INT, i, REQUEST_TO_JOIN, MPI_COMM_WORLD );
         }
       }
     }
+    free(main_loop_buffer_int);
   } else {
     get_random_id(&pair_id, SIZE_X, SIZE_Y);
     while(1) {
-      MPI_Recv(&node_number ,1 , MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &main_loop_status);
+      MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &main_loop_status);
+      MPI_Get_count (&main_loop_status, MPI_INT, &count);
       main_loop_from = main_loop_status.MPI_SOURCE;
       main_loop_tag  = main_loop_status.MPI_TAG;
+      main_loop_buffer_int = (int*) malloc(sizeof(int));
+      MPI_Recv(&main_loop_buffer_int[0] ,count , MPI_INT, main_loop_from, main_loop_tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       if(main_loop_tag == ROOT_TAG_INIT_NODE) {
         //initialisation of bootstrap node
-        if(bootstrap == false && node_number == 1) {
+        if(bootstrap == false && *main_loop_buffer_int == 1) {
           bootstrap   = true;
-          buffer_int = 0;
+          main_loop_buffer_int = 0;
           init_land(&land_id, 0, 0, SIZE_X, SIZE_Y);
-          MPI_Send(&buffer_int ,1 ,MPI_INT ,ROOT_PROCESS ,ACK_TAG ,MPI_COMM_WORLD);
+          MPI_Send(&main_loop_buffer_int ,1 ,MPI_INT ,ROOT_PROCESS ,ACK_TAG ,MPI_COMM_WORLD);
         } else {
-          MPI_Send(&buffer_int ,1 ,MPI_INT ,ROOT_PROCESS ,REQUEST_TO_JOIN ,MPI_COMM_WORLD);
+          MPI_Send(&main_loop_buffer_int ,1 ,MPI_INT ,ROOT_PROCESS ,REQUEST_TO_JOIN ,MPI_COMM_WORLD);
         }
       } else if(main_loop_tag == REQUEST_TO_JOIN) {
-        printf("voir avec %d pour s'insérer \n", node_number);
-      } else {
-          printf("unknow tag");
+        if(main_loop_from == 0) {
+          printf("voir avec %d pour s'insérer \n", *main_loop_buffer_int);
+          send_int_buffer = malloc(sizeof(int) * 3);
+          send_int_buffer[0] = com_rank; send_int_buffer[1] = pair_id.x; send_int_buffer[2] = pair_id.y;
+          MPI_Send(&(send_int_buffer[0]), 3, MPI_INT, *main_loop_buffer_int, LOCALIZE, MPI_COMM_WORLD);
+          MPI_Recv(&localise, 1, MPI_INT, MPI_ANY_SOURCE, LOCALIZE_RESP, MPI_COMM_WORLD, &main_loop_status);
+          MPI_Send(&(send_int_buffer[0]), 3 , MPI_INT, localise, REQUEST_TO_JOIN, MPI_COMM_WORLD);
+          printf("négocier avec le noeud %d \n", localise);
+          free(send_int_buffer); send_int_buffer = NULL;
+        } else {
+          printf("le noeud %d asked me to join \n", main_loop_buffer_int[0]);
+        }
+      } else if( main_loop_tag == LOCALIZE ) {
+        corresp = main_loop_buffer_int[0];
+        init_pair(&pair_join_request,  main_loop_buffer_int[1], main_loop_buffer_int[2]);
+        if(is_land_contains_pair(&land_id, &pair_join_request)) {
+          printf("its here ! \n");
+          MPI_Send(&com_rank, 1, MPI_INT, corresp, LOCALIZE_RESP, MPI_COMM_WORLD);
+        } else {
+          // trouver un voisin et transmettre la requete.
+        }
+      } else   {
+        printf("unknow tag \n");
       }
+    free(main_loop_buffer_int);
     }
   }
 
@@ -136,6 +175,12 @@ int main(int argc, char**argv) {
 void get_random_id(pair *p, int min, int max) {
   p->x = rand() % max;
   p->y = rand() % max;
+}
+
+
+void init_pair(pair *p, int x, int y) {
+  p->x = x;
+  p->y = y;
 }
 
 void print_pair(const pair *p) {
@@ -151,6 +196,10 @@ void init_land(land *l, int x, int y,unsigned int s_x,unsigned int s_y) {
 
 bool is_land_contains(const land *l, int x, int y) {
   return ((l->x < x && l->x + l->size_x > x) && (l->y < y && l->y + l->size_y > y));
+}
+
+bool is_land_contains_pair(const land *l,const pair *p) {
+  return ((l->x < p->x && l->x + l->size_x > p->x) && (l->y < p->y && l->y + l->size_y > p->y));
 }
 
 void split_land(land *new_land ,land *old_land) {
